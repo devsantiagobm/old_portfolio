472ca41b96b566613d66274996e14c482f7d7860
santiago <sanseb290514@gmail.com>
2022-01-07 17:02:38 -0500

    My first portfolio
    Basic portfolio built with HTML5, CSS3 and a bit of JavaScript

---
+++ js/index.js
@@ -0,0 +1,8 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.section__about, .section__skills, .section__proyecto, .section__formulario, .section__linea', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '100px',
+        delay: 30
+    });
